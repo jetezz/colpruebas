0c19a1b09ecf0fe7969dbb0ab24fa02ff6bc74ea
jetezz <cuadrabc2015@gmail.com>
2026-03-12 18:11:04 +0100

    main

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -4,6 +4,7 @@ export default defineConfig({
   output: 'static',
   server: {
     port: 4321,
-    host: true
+    host: true,
+    allowedHosts: ['colpruebas.online', 'localhost', '127.0.0.1'],
   }
 });
